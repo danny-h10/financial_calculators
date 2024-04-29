84c63c5cbc7eeca027d697fb4866f843b58aee13
Daniel Hernandez <dh893177@gmail.com>
2024-04-29 11:05:46 -0500

    added basic calc

---
+++ scripts/basicCalc.js
@@ -0,0 +1,71 @@
+"use strict"
+console.log("it is working!")
+
+
+window.onload = init;
+
+function init() {
+
+    const addButton = document.querySelector("#addButton");
+    const subtractButton = document.querySelector("#subtractButton");
+    const multiplyButton = document.querySelector("#multiplyButton");
+    const divideButton = document.querySelector("#divideButton");
+
+    addButton.addEventListener("click", add)
+    subtractButton.addEventListener("click", subtract)
+    multiplyButton.addEventListener("click", multiply)
+    divideButton.addEventListener("click", divide)
+}
+
+function add (){
+
+    //get the values out of the input fields teh uder types in
+    let number1 = Number( document.querySelector("#number1Field").value)
+    let number2 = Number( document.querySelector("#number2Field").value )
+
+    //add those number together
+    let result = number1 + number2;
+
+    //put hte rsult in the answer input field
+    document.querySelector("#answerField").value = result
+}
+
+function subtract (){
+
+    //get the values out of the input fields teh uder types in
+    let number1 = Number( document.querySelector("#number1Field").value)
+    let number2 = Number( document.querySelector("#number2Field").value )
+
+    //add those number together
+    let result = number1 - number2;
+
+    //put hte rsult in the answer input field
+    document.querySelector("#answerField").value = result
+}
+
+function multiply (){
+
+    //get the values out of the input fields teh uder types in
+    let number1 = Number( document.querySelector("#number1Field").value)
+    let number2 = Number( document.querySelector("#number2Field").value )
+
+    //add those number together
+    let result = number1 * number2;
+
+    //put hte rsult in the answer input field
+    document.querySelector("#answerField").value = result
+}
+
+
+function divide (){
+
+    //get the values out of the input fields teh uder types in
+    let number1 = Number( document.querySelector("#number1Field").value)
+    let number2 = Number( document.querySelector("#number2Field").value )
+
+    //add those number together
+    let result = number1 / number2;
+
+    //put hte rsult in the answer input field
+    document.querySelector("#answerField").value = result
+}
